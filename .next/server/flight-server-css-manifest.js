b45139548710f41524401be61287787ec2b9b9fc
masapasa <apyakurel@gmail.com>
2023-11-21 14:02:13 +0100

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,23 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/aswin/Documents/startup-nextjs/app/page": [
+      "/Users/aswin/Documents/startup-nextjs/node_modules/react-modal-video/css/modal-video.css",
+      "/Users/aswin/Documents/startup-nextjs/styles/index.css"
+    ],
+    "/Users/aswin/Documents/startup-nextjs/app/contact/page": [
+      "/Users/aswin/Documents/startup-nextjs/node_modules/react-modal-video/css/modal-video.css",
+      "/Users/aswin/Documents/startup-nextjs/styles/index.css"
+    ],
+    "/Users/aswin/Documents/startup-nextjs/app/signup/page": [
+      "/Users/aswin/Documents/startup-nextjs/node_modules/react-modal-video/css/modal-video.css",
+      "/Users/aswin/Documents/startup-nextjs/styles/index.css"
+    ]
+  },
+  "/Users/aswin/Documents/startup-nextjs/app/page.jsx": [
+    "/Users/aswin/Documents/startup-nextjs/node_modules/@next/font/google/target.css?{\"path\":\"app/page.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/aswin/Documents/startup-nextjs/app/layout.jsx": [
+    "/Users/aswin/Documents/startup-nextjs/node_modules/react-modal-video/css/modal-video.css",
+    "/Users/aswin/Documents/startup-nextjs/styles/index.css"
+  ]
+}
